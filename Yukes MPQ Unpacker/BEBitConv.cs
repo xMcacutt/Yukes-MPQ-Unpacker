b3a8567696ac7f314f20267cc15643d2e811453a
xMcacutt <matthew.cacutt@gmail.com>
2023-10-08 22:23:29 +0100

    Add project files.

---
+++ Yukes MPQ Unpacker/BEBitConv.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yukes_MPQ_Unpacker
+{
+    public static class BEBitConv
+    {
+        public unsafe static byte[] GetBytes(short value)
+        {
+            byte[] array = new byte[2];
+            fixed (byte* ptr = array)
+            {
+                ptr[0] = (byte)(value >> 8);    // Store the high byte (MSB) at index 0
+                ptr[1] = (byte)value;            // Store the low byte (LSB) at index 1
+            }
+            return array;
+        }
+
+        public unsafe static byte[] GetBytes(int value)
+        {
+            byte[] array = new byte[4];
+            fixed (byte* ptr = array)
+            {
+                ptr[0] = (byte)(value >> 24);   // Store the highest byte (MSB) at index 0
+                ptr[1] = (byte)(value >> 16);   // Store the second highest byte at index 1
+                ptr[2] = (byte)(value >> 8);    // Store the second lowest byte at index 2
+                ptr[3] = (byte)value;            // Store the lowest byte (LSB) at index 3
+            }
+            return array;
+        }
+
+        public unsafe static byte[] GetBytes(long value)
+        {
+            byte[] array = new byte[8];
+            fixed (byte* ptr = array)
+            {
+                ptr[0] = (byte)(value >> 56);
+                ptr[1] = (byte)(value >> 48);
+                ptr[2] = (byte)(value >> 40);
+                ptr[3] = (byte)(value >> 32);
+                ptr[4] = (byte)(value >> 24);
+                ptr[5] = (byte)(value >> 16);
+                ptr[6] = (byte)(value >> 8);
+                ptr[7] = (byte)value;
+            }
+            return array;
+        }
+
+        public static byte[] GetBytes(ushort value)
+        {
+            return GetBytes((short)value);
+        }
+
+        public static byte[] GetBytes(uint value)
+        {
+            return GetBytes((int)value);
+        }
+
+        public static byte[] GetBytes(ulong value)
+        {
+            return GetBytes((long)value);
+        }
+
+        public unsafe static byte[] GetBytes(float value)
+        {
+            return GetBytes(*(int*)(&value));
+        }
+
+        public unsafe static byte[] GetBytes(double value)
+        {
+            return GetBytes(*(long*)(&value));
+        }
+
+        public unsafe static int ToInt32(byte[] value, int startIndex)
+        {
+            fixed (byte* ptr = &value[startIndex])
+            {
+                if (Program.LittleEndian)
+                {
+                    return *ptr | (ptr[1] << 8) | (ptr[2] << 16) | (ptr[3] << 24);
+                }
+
+                return (*ptr << 24) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3];
+            }
+        }
+
+        public static uint ToUInt32(byte[] value, int startIndex)
+        {
+            return (uint)ToInt32(value, startIndex);
+        }
+    }
+}
---
+++ Yukes MPQ Unpacker/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Yukes_MPQ_Unpacker
+{
+    public class Program
+    {
+        static string _path;
+        static bool _run = true;
+        public static bool LittleEndian = false;
+
+        public static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                if (File.Exists(arg)) Extract(arg);
+            }
+            if (args.Length > 0) return;
+            while (_run)
+            {
+                Console.Clear();
+                Console.WriteLine("MPQ THP Extractor for the Wii Version of The DOG Island");
+                Console.WriteLine("Please enter path to MPQ");
+                _path = Console.ReadLine().Replace("\"", "");
+                while (!File.Exists(_path) || !_path.EndsWith(".mpq", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Console.WriteLine("Path was invalid");
+                    Console.WriteLine("Please enter path to MPQ");
+                    _path = Console.ReadLine().Replace("\"", " ");
+                }
+                Console.WriteLine("Extracting...");
+                Extract(_path);
+                Console.WriteLine("Extract completed successfully");
+                Console.ReadLine();
+            }
+        }
+
+        public static void Extract(string path)
+        {
+            string pacName = Path.GetFileNameWithoutExtension(path);
+            string inDirPath = Path.GetDirectoryName(path);
+            string outDirPath = Path.Combine(inDirPath, pacName + " Extract");
+            if (!Directory.Exists(outDirPath)) Directory.CreateDirectory(outDirPath);
+
+            var f = File.OpenRead(path);
+            f.Seek(0xC, SeekOrigin.Begin);
+            uint fileCount = BEBitConv.ToUInt32(ReadBytes(f, new byte[4]), 0);
+            List<uint> filePtrs = new List<uint>();
+            for(int i = 0; i < fileCount; i++)
+            {
+                filePtrs.Add(BEBitConv.ToUInt32(ReadBytes(f, new byte[4]), 0));
+            }
+            f.Seek(filePtrs[0], SeekOrigin.Begin);
+            string extension = Regex.Replace(Encoding.ASCII.GetString(ReadBytes(f, new byte[4])), @"[\x00-\x1F\x7F]", "");
+            for (int i = 0; i < fileCount; i++)
+            {
+                uint startPoint = filePtrs[i];
+                uint endPoint = i == fileCount - 1 ? (uint)f.Length : filePtrs[i + 1];
+                f.Seek(startPoint, SeekOrigin.Begin);
+                FileStream outFile = File.Create(Path.Combine(outDirPath, pacName + "_" + i + "." + extension));
+                outFile.Write(ReadBytes(f, new byte[endPoint - startPoint]));
+                outFile.Close();
+            }
+        }
+
+        public static byte[] ReadBytes(FileStream f, byte[] buffer)
+        {
+            f.Read(buffer, 0, buffer.Length);
+            return buffer;
+        }
+    }
+}
+
